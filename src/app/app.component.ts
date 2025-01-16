c26f2161716431d024ebbccd23bb06d12a2adbfb
CrazyDenzi <and.denz.22@lehre.mosbach.dhbw.de>
2025-01-16 21:50:56 +0100

    feat(dark-mode): extend dark mode functionality to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, ChangeDetectionStrategy, ChangeDetectorRef, AfterViewInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ChangeDetectorRef, AfterViewInit, Renderer2 } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router'; 
 import { AuthService } from './services/auth.service';
 import { NgIf } from '@angular/common';
-import { HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { DarkModeService } from './services/dark-mode.service';
 
 @Component({
   selector: 'an-root',  
@@ -34,10 +35,27 @@ import { HttpClientModule} from '@angular/common/http';
 export class AppComponent implements AfterViewInit {
   title = 'B2B-Webshop'; 
 
-  constructor(private authService: AuthService, private cdr: ChangeDetectorRef) {}
+  constructor(
+    private authService: AuthService, 
+    private cdr: ChangeDetectorRef,
+    private renderer: Renderer2,
+    private darkModeService: DarkModeService
+  ) {}
 
   get isLoggedIn(): boolean { 
     return this.authService.isLoggedIn(); 
+  }
+
+  ngOnInit(): void {
+    const isDarkModeEnabled = this.darkModeService.isDarkModeEnabled();
+    console.log('Dark Mode Status:', isDarkModeEnabled);
+    if (isDarkModeEnabled) {
+      this.renderer.addClass(document.body, 'dark-mode');
+      console.log('Dark Mode aktiviert:', document.body.classList);
+    } else {
+      this.renderer.removeClass(document.body, 'dark-mode');
+      console.log('Dark Mode deaktiviert:', document.body.classList);
+    }
   }
 
   ngAfterViewInit() {
