298bd0d794f12cbe47dd60e401de2f560d0cdf6c
CrazyDenzi <and.denz.22@lehre.mosbach.dhbw.de>
2025-01-16 21:59:18 +0100

    feat(dark-mode): extend dark mode functionality to about.component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { DarkModeService } from '../services/dark-mode.service';
 
 @Component({
-  selector: 'an-about',
+  selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css'] 
+  styleUrls: ['./about.component.css']
 })
-export class AboutComponent {}
+export class AboutComponent implements OnInit {
+
+  constructor(private darkModeService: DarkModeService, private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    const isDarkModeEnabled = this.darkModeService.isDarkModeEnabled();
+    console.log('Dark Mode Status:', isDarkModeEnabled);
+    if (isDarkModeEnabled) {
+      this.renderer.addClass(document.body, 'dark-mode');
+      console.log('Dark Mode aktiviert:', document.body.classList);
+    } else {
+      this.renderer.removeClass(document.body, 'dark-mode');
+      console.log('Dark Mode deaktiviert:', document.body.classList);
+    }
+  }
+}
