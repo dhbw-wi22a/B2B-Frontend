c4a21d0faec8b3bbae5caea63c8dfbaf2adfc78d
ste-spa <steve.spatzek@gmail.com>
2025-01-16 11:57:19 +0100

    fix:  api url

--- src/enviroments/environment.ts
+++ src/enviroments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    //apiUrl: 'http://hothardwarehubtest-shop-xco4pb-80b302-5-75-130-54.traefik.me/web/api'
-    apiUrl: 'http://localhost:8088/web/api'
+    apiUrl: 'http://hothardwarehubtest-shop-xco4pb-80b302-5-75-130-54.traefik.me/web/api'
+    //apiUrl: 'http://localhost:8088/web/api'
 };
